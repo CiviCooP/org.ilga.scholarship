4e0fb13d31202697e74180364a77f9dc04437d00
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2018-08-17 18:41:15 +0200

    Added parameter for scoring of a single application.

--- api/v3/Scholarship/Score.php
+++ api/v3/Scholarship/Score.php
@@ -23,14 +23,25 @@ function _civicrm_api3_scholarship_score_spec(&$spec) {
  */
 function civicrm_api3_scholarship_score($params) {
 
+  $returnValues = [];
+
+  if ($params['case_id']) {
     $dao = CRM_Core_DAO::executeQuery("
-      select id from civicrm_case where case_type_id = %1",array(
-      1 => array(variable_get(ILGA_SCHOLARSHIP_CASE_TYPE),'Integer')
-    ));
-    while($dao->fetch()){
-      $score = new CRM_Scholarship_Score($dao->id);
-      $score->process();
-    }
-    $returnValues = [];
-    return civicrm_api3_create_success($returnValues, $params, 'Scholarship', 'Score');
+      select id from civicrm_case where case_type_id = %1 and id = %2", [
+      1 => [variable_get(ILGA_SCHOLARSHIP_CASE_TYPE), 'Integer'],
+      2 => [$params['case_id'], 'Integer'],
+    ]);
+    $returnValues['case_id'] = $params['case_id'];
+  }
+  else {
+    $dao = CRM_Core_DAO::executeQuery("
+        select id from civicrm_case where case_type_id = %1", [
+      1 => [variable_get(ILGA_SCHOLARSHIP_CASE_TYPE), 'Integer'],
+    ]);
+  }
+  while ($dao->fetch()) {
+    $score = new CRM_Scholarship_Score($dao->id);
+    $score->process();
+  }
+  return civicrm_api3_create_success($returnValues, $params, 'Scholarship', 'Score');
 }
